30fe91f1a96990a675acea6a21fd2a03e2cbc53a
Dillywink0 <dillon140503@gmail.com>
2024-06-14 15:50:44 +0100

    1

--- src/pages/api/scrape.js
+++ src/pages/api/scrape.js
@@ -2,7 +2,7 @@
 import { scrapeAmazonPage } from '../../lib/scraper';
 
 export default async function handler(req, res) {
-  const { urls } = req.body; // Assuming URLs are sent in the request body as an array
+  const { urls } = req.body;
 
   if (!urls || !Array.isArray(urls) || urls.length === 0) {
     return res.status(400).json({ error: 'Invalid URLs provided' });
@@ -11,12 +11,14 @@ export default async function handler(req, res) {
   try {
     const scrapePromises = urls.map(url => scrapeAmazonPage(url));
     const productsArray = await Promise.all(scrapePromises);
-    
+
     // Flatten the products array (if needed)
     const products = productsArray.flat();
-    
+
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error('Error scraping:', error); // Log the error for debugging purposes
+
+    res.status(500).json({ error: 'Failed to scrape Amazon pages' });
   }
 }
